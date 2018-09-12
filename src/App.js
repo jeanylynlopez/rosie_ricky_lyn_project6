4bab5623aa90960cb3d807670fc1b18d0ca74a6a
Jeanylyn <jeanylynlopez@gmail.com>
2018-09-11 22:20:51 -0400

    making sure it's getting the right amount of numbers

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
 
     axios.get("https://opentdb.com/api.php?", {
       params: {
-        amount: 50,
+        amount: 10,
         category: category,
         difficulty: difficulty,
         // type: "multiple"
